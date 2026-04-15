a178b32e27657055cbaf647c910ec2a6aadb104e
Louis Marotta <loumb31@gmail.com>
2026-04-15 19:14:36 +0200

    Basename on `CI=true`

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,11 @@ import i18n from "@mannisto/astro-i18n";
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
+let base = process?.env?.CI 
+    ? '/m7-jellyfin' 
+    : '';
 export default defineConfig({
+    base: base,
     integrations: [
         i18n({
             defaultLocale: "en",
